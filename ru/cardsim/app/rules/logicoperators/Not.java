ec44479b73a34623efbf90355aa902e1f3abcc24
LazyTroll <diahorver@gmail.com>
2016-02-12 20:02:11 +0400

    Добавил класс Result, как еще один логический оператор

--- ru/cardsim/app/rules/logicoperators/Not.java
+++ ru/cardsim/app/rules/logicoperators/Not.java
@@ -9,5 +9,4 @@ public class Not extends LogicOperator {
         setResult(!value);
     }
 
-
 }
---
+++ ru/cardsim/app/rules/logicoperators/Result.java
@@ -0,0 +1,7 @@
+package ru.cardsim.app.rules.logicoperators;
+
+public class Result extends LogicOperator {
+    public Result(boolean value) {
+        setResult(value);
+    }
+}
